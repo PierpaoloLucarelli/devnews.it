fd55e043c6261617fc5ffae3efd69f9d09807cd1
Pierpaolo <plucarelli.94@gmail.com>
2017-01-26 21:03:09 +0000

    aggiunto guida react

--- reactTestApp/src/index.js
+++ reactTestApp/src/index.js
@@ -1,2 +1,12 @@
-let ciao = "HELLO WORLD";
-console.log(ciao);
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+    render(){
+        return(
+            <h1>Hello World</h1>
+        )
+    }
+}
+
+render(<App />, document.getElementById('app'));
