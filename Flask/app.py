55bbcc019ca4ef3b4dbd70741e99cc84e26eadfd
Pierpaolo <plucarelli.94@gmail.com>
2017-12-16 16:39:10 +0000

    aggiunto guida flask

---
+++ Flask/app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route("/")
+def hello():
+	return render_template('index.html', nome="marco")
+if __name__ == "__main__":
+	app.run()
+
